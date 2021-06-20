97ef7564adc3be8717b4b0ce1cf44f8b28c116aa
vlt80 <vlt80@DESKTOP-6PKJH38>
2021-06-20 14:13:58 +0900

    2021.06.20

---
+++ wonbin/src/main/java/com/example/wonbin/controller/MainController.java
@@ -0,0 +1,5 @@
+package com.example.wonbin.controller;
+
+public class MainController {
+
+}
---
+++ wonbin/src/test/java/com/example/wonbin/WonbinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wonbin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonbinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
